73dc14e34ff867773be3d8123f4bfa1dbee87003
smallbee <smallbee3@gmail.com>
2019-01-07 07:14:57 +0900

    Board Create API done

--- app/boards/apis.py
+++ app/boards/apis.py
@@ -14,3 +14,6 @@ class BoardListCreateAPIView(generics.ListCreateAPIView):
     permission_classes = (
         permissions.IsAuthenticated,
     )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
