e6a42ae155218dec0fbae8b3a12e81f6b4b18cc3
smallbee <smallbee3@gmail.com>
2019-01-07 05:02:36 +0900

    boards app added

---
+++ app/boards/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+__all___ = (
+    'Board',
+)
+
+
+class Board(models.Model):
+    title = models.CharField(
+        max_length=150,
+        verbose_name="Board's title",
+    )
+    bgColor = models.CharField(
+        max_length=50,
+        verbose_name="Board's background color",
+        blank=True
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    created_date = models.DateField(
+        verbose_name='Created date',
+        auto_now_add=True,
+    )
+    modified_date = models.DateField(
+        verbose_name='Modified date',
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f'{self.pk} {self.title}'
